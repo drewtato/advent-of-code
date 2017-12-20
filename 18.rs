f2e2a3e4f4b029c33a762ec98eb82dcbfee9994e
drewtato <a.irino.42@gmail.com>
2017-12-20 15:52:56 -0800

    day 18 part 2 not working still

--- 18.rs
+++ 18.rs
@@ -2,6 +2,7 @@
 
 use std::fs;
 use std::io::Read;
+use std::collections::VecDeque;
 
 fn main() {
 	let mut input = String::new();
@@ -11,7 +12,7 @@ fn main() {
 		input.pop();
 	}
 
-	let instructions: Vec<(char, char, Result<char, isize>)> = input.lines().map(|x| {
+	let instructions: Vec<(char, Result<char, isize>, Result<char, isize>)> = input.lines().map(|x| {
 		let ins = x.chars().nth(1).unwrap();
 		let reg = x.chars().nth(4).unwrap();
 		let count = x.chars().skip(6).collect::<String>();
@@ -21,6 +22,12 @@ fn main() {
 				0 => Err(0),
 				1 if a.unwrap().is_alphabetic() => Ok(a.unwrap()),
 				_ => Err(count.parse::<isize>().unwrap()),
+			}
+		};
+		let reg = {
+			match reg {
+				x if x.is_alphabetic() => Ok(x),
+				x => Err((x as isize) - ('a' as isize)),
 			}
 		};
 		(ins, reg, count)
@@ -34,7 +41,10 @@ fn main() {
 
 	loop {
 		let (i, reg, count) = instructions[current as usize];
-		let reg = (reg as usize) - ('a' as usize);
+		let (reg, num) = match reg {
+			Ok(x) =>((x as usize) - ('a' as usize), 0),
+			Err(x) => ('z' as usize, x),
+		};
 		let count = match count {
 			Ok(x) => regs[(x as usize) - ('a' as usize)],
 			Err(x) => x,
@@ -49,8 +59,13 @@ fn main() {
 				if regs[reg] != 0 {break;}
 			},
 			'g' => {
-				if regs[reg] > 0 {
-					current += count - 1;
+				match (reg, num) {
+					(x, 0) =>{
+						if regs[x] > 0 {
+							current += count - 1
+						}
+					},
+					_ => current += count - 1,
 				}
 			},
 			x @ _ => panic!("not a real ins: {}", x),
@@ -59,4 +74,102 @@ fn main() {
 		if (current > instructions.len() as isize) || (current < 0) {break;}
 	}
 	println!("{}", sound);
+
+	let mut queue1: VecDeque<isize>= VecDeque::new();
+	let mut queue2: VecDeque<isize>= VecDeque::new();
+	let mut current1: isize = 0;
+	let mut current2: isize = 0;
+	let mut reg1: Vec<isize> = vec![0; 26];
+	let mut reg2: Vec<isize> = vec![0; 26];
+	let mut ans = 0;
+	reg2[('p' as usize) - ('a' as usize)] = 1;
+
+	loop {
+		if (current1 >= instructions.len() as isize) || (current1 < 0) {break;}
+	    let (i, reg, count) = instructions[current1 as usize];
+		println!("1: Begin {} at {}", i, current1);
+		let (reg, num) = match reg {
+			Ok(x) =>((x as usize) - ('a' as usize), 0),
+			Err(x) => ('z' as usize, x),
+		};
+		let count = match count {
+			Ok(x) => reg1[(x as usize) - ('a' as usize)],
+			Err(x) => x,
+		};
+		match i {
+			'n' => {
+				queue2.push_back(count);
+				ans += 1;
+			},
+			'e' => reg1[reg] = count,
+			'd' => reg1[reg] += count,
+			'u' => reg1[reg] *= count,
+			'o' => reg1[reg] %= count,
+			'c' => {
+
+				// Program 2 runs
+
+				loop {
+					if (current2 >= instructions.len() as isize) || (current2 < 0) {break;}
+				    let (i, reg, count) = instructions[current2 as usize];
+					println!("2: Begin {} at {}", i, current2);
+					let (reg, num) = match reg {
+						Ok(x) =>((x as usize) - ('a' as usize), 0),
+						Err(x) => ('z' as usize, x),
+					};
+					let count = match count {
+						Ok(x) => reg2[(x as usize) - ('a' as usize)],
+						Err(x) => x,
+					};
+					match i {
+						'n' => queue1.push_back(count),
+						'e' => reg2[reg] = count,
+						'd' => reg2[reg] += count,
+						'u' => reg2[reg] *= count,
+						'o' => reg2[reg] %= count,
+						'c' => {
+							reg2[reg] = match queue2.pop_front() {
+								Some(x) => x,
+								None => break,
+							}
+						},
+						'g' => {
+							match (reg, num) {
+								(x, 0) =>{
+									if reg2[x] > 0 {
+										current2 += count - 1
+									}
+								},
+								_ => current2 += count - 1,
+							}
+						},
+						x @ _ => panic!("not a real ins: {}", x),
+					}
+					current2 += 1;
+
+					// Program 2 ends
+
+				}
+				reg1[reg] = match queue1.pop_front() {
+					Some(x) => x,
+					None => break,
+				}
+
+
+			},
+			'g' => {
+				match (reg, num) {
+					(x, 0) =>{
+						if reg1[x] > 0 {
+							current1 += count - 1
+						}
+					},
+					_ => current1 += count - 1,
+				}
+			},
+			x @ _ => panic!("not a real ins: {}", x),
+		}
+		current1 += 1;
+	}
+	println!("{}", ans);
 }
